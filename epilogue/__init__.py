5f52b7eaa0cb2d6dab1848a013945ed22390faab
DaJour Christophe <74025275+DaJourJChristophe@users.noreply.github.com>
2026-08-16 14:26:26 -0600

    Make observability Python-only

---
+++ epilogue/__init__.py
@@ -0,0 +1,7 @@
+from .batch   import BatchMonitor
+from .monitor import StackMonitor
+
+__all__ = [
+    'BatchMonitor',
+    'StackMonitor',
+]
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='epilogue',
+    version='0.1.0',
+    packages=find_packages(),
+    python_requires='>=3.10',
+    zip_safe=False,
+)
